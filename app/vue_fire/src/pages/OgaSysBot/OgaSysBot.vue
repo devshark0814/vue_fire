af890a570496317bf6e0fa76547fbc26ae93504e
devshark0814 <devshark0814@gmail.com>
2021-09-23 12:07:54 +0900

    レイアウト修正

--- app/vue_fire/src/pages/OgaSysBot/OgaSysBot.vue
+++ app/vue_fire/src/pages/OgaSysBot/OgaSysBot.vue
@@ -1,8 +1,11 @@
 <template>
-    <iframe
-        allow="microphone;"
-        width="350"
-        height="430"
-        src="https://console.dialogflow.com/api-client/demo/embedded/18f430c1-3370-4661-b2ea-089f370c7017">
-    </iframe>
+    <div style="position: relative;">
+        <iframe
+            style="position: absolute;top: 50%;left: 24%;"
+            allow="microphone;"
+            width="900"
+            height="800"
+            src="https://console.dialogflow.com/api-client/demo/embedded/18f430c1-3370-4661-b2ea-089f370c7017">
+        </iframe>
+    </div>
 </template>
